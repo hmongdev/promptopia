dfb14f03f736487832d54565584dde9546dd237d
Avery Yang <averyyang2674@gmail.com>
2023-08-16 19:44:33 -0500

    building navbar.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 const RootLayout = ({ children }) => {
 	return (
@@ -8,7 +10,10 @@ const RootLayout = ({ children }) => {
 					<div className="gradient" />
 				</div>
 
-				<main className="app">{children}</main>
+				<main className="app">
+					<Nav />
+					{children}
+				</main>
 			</body>
 		</html>
 	);
---
+++ components/Nav.jsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+
+const Nav = () => {
+	return (
+		<nav className="flex-between w-full mb-16 pt-3">
+			<Link href="/" className="flex gap-2 flex-center">
+				<Image
+					src="/assets/images/logo.svg"
+					width={30}
+					height={30}
+				/>
+			</Link>
+		</nav>
+	);
+};
+
+export default Nav;
