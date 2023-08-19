f986797439ac2a3845b33cfa21696706ff88ee97
Avery Yang <averyyang2674@gmail.com>
2023-08-19 17:43:34 -0500

    create user model

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
+import { connectToDB } from '@utils/database';
+
 const handler = NextAuth({
 	providers: [
 		GoogleProvider({
@@ -9,5 +11,18 @@ const handler = NextAuth({
 		}),
 	],
 	async session({ session }) {},
-	async signIn({ profile }) {},
+	async signIn({ profile }) {
+		try {
+			await connectToDB();
+
+			// check if a user already exists
+
+			// if not, create a new user
+
+			return true;
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+	},
 });
